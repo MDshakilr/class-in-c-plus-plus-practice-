c48b53960d29b766268851848bae6d30a2b9867f
Md. Shakil Rana <159621858+MDshakilr@users.noreply.github.com>
2024-02-19 02:38:43 +0600

    Update 1.cpp

--- class in c++ practice/Problem 1 .Library Management System/1.cpp
+++ class in c++ practice/Problem 1 .Library Management System/1.cpp
@@ -1,3 +1,19 @@
+/*Create a class `Book` with attributes `title`, `author`, and `ISBN`. Implement a parameterized
+constructor to initialize these attributes. Write a destructor to display a message when a book
+object is destroyed. Create multiple book objects and demonstrate constructor and destructor
+calls.
+Sample Input:
+Book 1: Title - "Introduction to OOP", Author - "John Doe", ISBN - "123456789"
+Book 2: Title - "Data Structures and Algorithms", Author - "Jane Smith", ISBN - "987654321"
+
+Sample Output:
+Book title created.
+Book title created.
+Book title destroyed.
+Book  title destroyed.
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 class Book{
